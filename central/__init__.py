b64087277a41e243c61d83f9a2f476763ed5086f
Rei-ath <hk350456@gmail..com>
2025-09-29 19:39:50 +0530

    Stop tracking large binaries

--- central/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Noctics Central core package."""
-
-from .cli import main, parse_args  # noqa: F401
-from .version import __version__  # noqa: F401
-
-__all__ = ["main", "parse_args", "__version__"]
